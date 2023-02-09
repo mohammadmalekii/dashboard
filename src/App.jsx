30ef48129edd5f521d99af95fa41be318aff160d
mohammad maleki <1mohamad.maleki@gmail.com>
2023-02-09 19:16:50 +0330

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="App bg-slate-300">
+    <main className="App bg-slate-300 h-screen">
       <Header />
-      <h1>Dashboard</h1>
+      <div className="flex justify-between pt-[88px]">
+        <h1>Dashboard</h1>
+        <Sidebar />
+      </div>
     </main>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import { Squares2X2Icon } from "@heroicons/react/24/outline";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import { NewspaperIcon } from "@heroicons/react/24/outline";
+import { UserPlusIcon } from "@heroicons/react/24/outline";
+import { CubeIcon } from "@heroicons/react/24/outline";
+
+export default function Sidebar() {
+  return (
+    <aside className="bg-white dark:bg-zinc-700 w-56 h-screen relative">
+      <ul className="flex flex-col gap-5 p-3.5 pl-4">
+        <li className="flex justify-end items-center gap-2 hover:bg-slate-100 hover:border-r-4 border-blue-300 hover:rounded-lg active:border-blue-700 transition-all duration-200 rounded-lg p-2 cursor-pointer dark:text-white dark:hover:bg-zinc-600 dark:hover:border-zinc-900 dark:active:border-blue-500">
+          <p>داشبورد</p>
+          <Squares2X2Icon className="h-7 w-7 text-slate-600 dark:text-white" />
+        </li>
+        <li className="flex justify-end items-center gap-2 hover:bg-slate-100 hover:border-r-4 border-blue-300 hover:rounded-lg active:border-blue-700 transition-all duration-200 rounded-lg p-2 cursor-pointer  dark:text-white dark:hover:bg-zinc-600 dark:hover:border-zinc-900 dark:active:border-blue-500">
+          <p>پروفایل</p>
+          <UserCircleIcon className="h-7 w-7 text-slate-600 dark:text-white" />
+        </li>
+        <li className="flex justify-end items-center gap-2 hover:bg-slate-100 hover:border-r-4 border-blue-300 hover:rounded-lg active:border-blue-700 transition-all duration-200 rounded-lg p-2 cursor-pointer dark:text-white dark:hover:bg-zinc-600 dark:hover:border-zinc-900 dark:active:border-blue-500">
+          <span className="inline-flex items-center justify-center w-4 h-4 text-xs text-white bg-blue-500 rounded-full top-0 -right-1">
+            {Number(3).toLocaleString("fa")}
+          </span>
+          <p>گفتگو ها</p>
+          <ChatBubbleLeftRightIcon className="h-7 w-7 text-slate-600 dark:text-white" />
+        </li>
+        <li className="flex justify-end items-center gap-2 hover:bg-slate-100 hover:border-r-4 border-blue-300 hover:rounded-lg active:border-blue-700 transition-all duration-200 rounded-lg p-2 cursor-pointer  dark:text-white dark:hover:bg-zinc-600 dark:hover:border-zinc-900 dark:active:border-blue-500">
+          <p>ارسال دعوت نامه</p>
+          <EnvelopeIcon className="h-7 w-7 text-slate-600 dark:text-white" />
+        </li>
+        <li className="flex justify-end items-center gap-2 hover:bg-slate-100 hover:border-r-4 border-blue-300 hover:rounded-lg active:border-blue-700 transition-all duration-200 rounded-lg p-2 cursor-pointer  dark:text-white dark:hover:bg-zinc-600 dark:hover:border-zinc-900 dark:active:border-blue-500">
+          <p>صدور پیش فاکتور</p>
+          <NewspaperIcon className="h-7 w-7 text-slate-600 dark:text-white" />
+        </li>
+        <li className="flex justify-end items-center gap-2 hover:bg-slate-100 hover:border-r-4 border-blue-300 hover:rounded-lg active:border-blue-700 transition-all duration-200 rounded-lg p-2 cursor-pointer  dark:text-white dark:hover:bg-zinc-600 dark:hover:border-zinc-900 dark:active:border-blue-500">
+          <p>عضویت در کانتمو</p>
+          <UserPlusIcon className="h-7 w-7 text-slate-600 dark:text-white" />
+        </li>
+        <li className="flex justify-end items-center gap-2 hover:bg-slate-100 hover:border-r-4 border-blue-300 hover:rounded-lg active:border-blue-700 transition-all duration-200 rounded-lg p-2 cursor-pointer  dark:text-white dark:hover:bg-zinc-600 dark:hover:border-zinc-900 dark:active:border-blue-500">
+          <p>نمایش آثار</p>
+          <CubeIcon className="h-7 w-7 text-slate-600 dark:text-white" />
+        </li>
+      </ul>
+    </aside>
+  );
+}
