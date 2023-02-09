06f81ffa0459a64126a5198c61bede8ccb78da00
mohammad maleki <1mohamad.maleki@gmail.com>
2023-02-09 16:58:53 +0330

    added dark mode button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import { BellIcon } from "@heroicons/react/24/outline";
 import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
 import logo from "../assets/logo.svg";
+import DarkModeButton from "./DarkModeButton";
 
 export default function Header() {
   return (
     <header className="w-full bg-white dark:bg-zinc-700 flex justify-between items-center px-7 py-5 shadow-lg">
       <div className="flex gap-5">
+        <DarkModeButton />
         <ArrowRightOnRectangleIcon className="h-9 w-9 text-slate-600 border border-slate-300 rounded-full p-[6px] cursor-pointer hover:bg-slate-300 dark:text-white dark:hover:text-slate-800 duration-200 transition-colors" />
         <Cog6ToothIcon className="h-9 w-9 text-slate-600 border border-slate-300 rounded-full p-[6px] cursor-pointer hover:bg-slate-300 dark:text-white dark:hover:text-slate-800 duration-200 transition-colors" />
         <button className="relative">
